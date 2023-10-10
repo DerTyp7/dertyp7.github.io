c0b8e19ef0a2fbfd38ab12426b7e51ca49823986
DerTyp7 <janis@tealfire.de>
2023-10-10 16:20:58 +0200

    implement responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,22 @@ import ThemeSwitch from "@components/ThemeSwitch";
 export default function Header() {
 	return (
 		<header className="header">
+			{/* Mobile Top Row Header */}
+			<div className="header-mobile">
+				<div className="logo">
+					<h1>Meister</h1>
+				</div>
+				<div className="control">
+					<ThemeSwitch />
+					<div>Language</div>
+				</div>
+			</div>
+			{/* END: Mobile Top Row Header */}
+
 			<div className="logo">
 				<h1>Meister</h1>
 			</div>
+
 			<div className="links">
 				<a href="">About</a>
 				<a href="">Skills</a>
