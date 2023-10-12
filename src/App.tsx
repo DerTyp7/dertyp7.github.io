2b48965891af57be57b50817458766d44d1921cf
DerTyp7 <janis@tealfire.de>
2023-10-12 15:39:54 +0200

    add about component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "@styles/App.scss";
 import Header from "@components/Header";
+import About from "@components/About";
 
 export default function App() {
 	return (
 		<div id="app">
 			<Header />
+			<About />
 		</div>
 	);
 }
